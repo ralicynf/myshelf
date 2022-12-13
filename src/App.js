0cf5c5035b3782e80ac4c8121f8937a49a09e340
rebecca <fulkerson.rebecca@gmail.com>
2022-12-13 12:46:53 -0500

    added auth to app.js and setup frontend routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Book from './pages/Book';
+import AddBook from './components/AddBook';
+import { Routes, Route } from 'react-router-dom'
+import { CheckSession } from './services/Auth';
+import { useState, useEffect } from 'react'
+
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  const handleLogout = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+
   return (
     <div className="App">
-      
+      <NavBar user={user} handleLogout={handleLogout}/>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home user={user} setUser={setUser} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route path="/addbook" element={<AddBook user={user} setUser={setUser} />} />
+          <Route path="/book/:id" element={<Book user={user} />} />
+        </Routes>
+      </main>
     </div>
   );
 }
