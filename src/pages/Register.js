4d138cf3fd8402b2dd37498233c51689385ca976
rebecca <fulkerson.rebecca@gmail.com>
2022-12-13 14:52:04 -0500

    built out register page; tested and functional (new users show up in backend)

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,94 @@
-
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { RegisterUser } from '../services/Auth'
 
 const Register = () => {
+    let navigate = useNavigate()
 
+    const initialState = {
+        username: '',
+        name: '',
+        email: '',
+        password: ''
+    }
+
+    const [formState, setFormState] = useState(initialState)
+
+    const handleChange = (e) => {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await RegisterUser({
+          username: formState.username,
+          name: formState.name,
+          email: formState.email,
+          password: formState.password
+        })
+        setFormState(initialState)
+        navigate('/login')
+      } 
+
+    return (
+        <div>
+            <div>
+                <form className='column' onSubmit={handleSubmit}>
+                    <div className='input-fields'>
+                        <label htmlFor='username'>Username   </label>
+                        <input
+                            className='input-reg'
+                            onChange={handleChange}
+                            name='username'
+                            type='text'
+                            placeholder='username'
+                            value={formState.username}
+                            required
+                        />
+                    </div>
+                    <div className='input-fields'>
+                        <label htmlFor='name'>Name   </label>
+                        <input
+                            className='input-reg'
+                            onChange={handleChange}
+                            name='name'
+                            type='text'
+                            placeholder='name'
+                            value={formState.name}
+                            required
+                        />
+                    </div>
+                    <div className='input-fields'>
+                        <label htmlFor='email'>Email   </label>
+                        <input
+                            className='input-reg'
+                            onChange={handleChange}
+                            name='email'
+                            type='text'
+                            placeholder='email'
+                            value={formState.email}
+                            required
+                        />
+                    </div>
+                    <div className='input-fields'>
+                        <label htmlFor='password'>Password   </label>
+                        <input
+                            className='input-reg'
+                            onChange={handleChange}
+                            name='password'
+                            type='text'
+                            placeholder='password'
+                            value={formState.password}
+                            required
+                        />
+                    </div>
+                    <div className='btn'>
+                        <button id='register-btn' type='submit'>Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Register
---
+++ src/services/Auth.js
@@ -0,0 +1,30 @@
+import Client from "./api";
+
+export const LoginUser = async (data) => {
+    try {
+      const res = await Client.post('/auth/login', data)
+      localStorage.setItem('token', res.data.token)
+      return res.data.user
+    } catch (error) {
+      return null
+    }
+  }
+  
+  export const RegisterUser = async (data) => {
+    try {
+      const res = await Client.post('/auth/register', data)
+      return res.data
+    } catch (error) {
+      throw error
+    }
+  }
+  
+  export const CheckSession = async () => {
+    try {
+      const res = await Client.get('/auth/session')
+      return res.data
+    } catch (error) {
+      throw error
+    }
+  }
+  
