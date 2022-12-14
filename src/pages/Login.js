558443e949c8111ca56c579b4144722e05794ce2
rebecca <fulkerson.rebecca@gmail.com>
2022-12-13 22:28:46 -0500

    added functionality to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,70 @@
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { LoginUser } from '../services/Auth'
+
+const Login = ({ setUser }) => {
+    let navigate = useNavigate()
+
+    const initialState = {
+        email: '',
+        password: ''
+    }
+
+    const [formState, setFormState] = useState(initialState)
+
+    const handleChange = (e) => {
+        setFormState({ ...formState, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const payload = await LoginUser(formState)
+        if (payload) {
+            await setUser(payload)
+            setFormState(initialState)
+            navigate('/')
+        } else {
+            window.alert('Your username or password is incorrect. Try again.')
+            setFormState({ email: formState.email, password: '' })
+    }
+    }
 
 
-const Login = () => {
-
+    return (
+        <div>
+            <div>
+                <form className="login-box" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email">Email   </label>
+                        <input 
+                            className='login-input'
+                            onChange={handleChange}
+                            name='email'
+                            type='text'
+                            placeholder='email'
+                            value={formState.email}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password   </label>
+                        <input 
+                            className='login-input'
+                            onChange={handleChange}
+                            name='password'
+                            type='password'
+                            placeholder='password'
+                            value={formState.password}
+                            required
+                        />
+                    </div>
+                    <div className='btn'>
+                        <button id='login-btn'>Login!</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Login
