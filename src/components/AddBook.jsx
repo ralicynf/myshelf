8dc1bddb2b1858b452044001c68508583b0b9fdd
rebecca <fulkerson.rebecca@gmail.com>
2022-12-14 12:30:51 -0500

    added services to src/services/book

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 const AddBook = () => {
---
+++ src/services/Book.js
@@ -0,0 +1,37 @@
+import Client from "./api";
+
+export const CreateBook = async (formData) => {
+    try {
+        const res = await Client.post('/books', formData)
+        return res.send
+    } catch (error) {
+        throw error
+    }
+}
+
+export const GetBooks = async () => {
+    try {
+        const res = await Client.get('/books')
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const UpdateBook = async (bookId, update) => {
+    try {
+        const res = await Client.put(`/books/${bookId}`, update)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const DeleteBook = async (bookId) => {
+    try {
+        const res = await Client.delete(`/books/${bookId}`)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
